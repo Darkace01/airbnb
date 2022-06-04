438c8f9619772352d3d619f9fa60d60717a6c88d
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-04 18:07:10 +0100

    Small cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import SmallCard from '../components/SmallCard';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ exploreData }) => {
   return (
     <div className=''>
       <Head>
@@ -15,10 +16,31 @@ const Home: NextPage = () => {
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(({ location, img, distance }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
   );
 };
 
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
+}
 export default Home;
