106f92fb4d28110f560e09a67cab449bd5f90a18
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-08 06:16:58 +0100

    Info card and search page

--- pages/search.js
+++ pages/search.js
@@ -3,8 +3,9 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useRouter } from 'next/router';
 import { format } from 'date-fns';
+import InfoCard from '../components/InfoCard';
 
-const Search = () => {
+const Search = ({ searchResults }) => {
   const router = useRouter();
   const { location, startDate, endDate, noOfGuests } = router.query;
   console.log(startDate);
@@ -32,6 +33,22 @@ const Search = () => {
             <p className='button'>Rooms and Beds</p>
             <p className='button'>More filters</p>
           </div>
+          <div className='flex flex-col'>
+            {searchResults?.map(
+              ({ img, title, description, price, star, total, location }) => (
+                <InfoCard
+                  key={img}
+                  title={title}
+                  description={description}
+                  img={img}
+                  price={price}
+                  star={star}
+                  total={total}
+                  location={location}
+                />
+              )
+            )}
+          </div>
         </section>
       </main>
       <Footer />
@@ -40,3 +57,14 @@ const Search = () => {
 };
 
 export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch('https://links.papareact.com/isz').then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
